3c83d130e04f1ab32edec3fc46217b95e99791f2
wow1548 <wow1548@naver.com>
2020-04-27 21:19:45 +0900

    Create models: course, content, review

--- Joodemy/Joodemy/settings.py
+++ Joodemy/Joodemy/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'rest_auth.registration',
 
     'accounts',
+    'course'
 ]
 
 MIDDLEWARE = [
---
+++ Joodemy/course/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from accounts.models import Instructor, User
+
+
+class Course(models.Model):
+    instructor = models.ForeignKey(
+        Instructor, on_delete=models.CASCADE, related_name="courses")
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    img = models.URLField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Content(models.Model):
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="contents")
+    title = models.CharField(max_length=64)
+    video = models.URLField()
+    time = models.TimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Review(models.Model):
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="reviews")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews")
+    rating = models.PositiveSmallIntegerField()
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
