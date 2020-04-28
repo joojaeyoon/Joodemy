b70f84fb24041e74784e6c18fef93d80b79ee7aa
wow1548 <wow1548@naver.com>
2020-04-28 18:55:47 +0900

    Create course test case

---
+++ Joodemy/api/tests/test_course.py
@@ -0,0 +1,59 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from django.contrib.auth import get_user_model
+
+from accounts.models import Instructor
+from course.models import Course
+
+
+class TestCourse(APITestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_instructor", email="test@test.com", password="password", is_instructor=True)
+        self.instructor = Instructor.objects.filter(user=self.user)[0]
+
+        self.course1 = Course.objects.create(instructor=self.instructor,
+                                             title="test course1", description="desciption",
+                                             price=19.99, img="http://test.jpg")
+        self.course2 = Course.objects.create(instructor=self.instructor,
+                                             title="test course2", description="desciption",
+                                             price=29.99, img="http://test.jpg")
+
+    def test_get_course_list(self):
+        """ 강의 리스트 API """
+
+        url = "/api/courses/"
+
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+
+    def test_create_course(self):
+        """ 강의 생성 API """
+
+        url = "/api/courses/"
+
+        payload = {
+            "instructor": self.instructor.id,
+            "title": "test course1",
+            "description": "desciption",
+            "price": 19.99,
+            "img": "http://test.jpg"
+        }
+
+        res = self.client.post(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data["instructor"], self.instructor.user.username)
+
+    def test_delete_course(self):
+        """ 강의 삭제 API """
+
+        url = "/api/courses/"+str(self.course2.id)
+
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
